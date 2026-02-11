439e5b8554023c350d8b95ee004edd6775c64ac9
DavinD <teswayae@gmail.com>
2026-02-11 14:37:08 +0700

    test

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Davin Dick - 00000108484</Text>
+      
+    </View>
+  );
+}
